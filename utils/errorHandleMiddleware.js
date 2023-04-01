271047e6664faf9316fdeb134ebbda03822cfc8d
jovanibrasil <jovanibrasil@gmail.com>
2023-04-01 20:13:04 -0300

    feat: adjust error middleware to better error handle
    - return generatic error when happens an operational error in prodution
    - return err and err stack when in development
    - return only status and message for other environments

--- utils/errorHandleMiddleware.js
+++ utils/errorHandleMiddleware.js
@@ -1,10 +1,20 @@
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
+
+  console.error(err);
+
+  let response = {
     status: err.status,
     message: err.message,
-    stack: err.stack,
-  });
+    ...(process.env.NODE_ENV === 'development' && { err, stack: err.stack }),
+  };
+
+  if (err.isOperational && process.env.NODE_ENV === 'production')
+    response = {
+      status: 500,
+      message: 'Sorry, something bad happened!',
+    };
+
+  res.status(err.statusCode).json(response);
 };
