86210ac88b21c62c2950a78048fa0e33423f8d3a
jovanibrasil <jovanibrasil@gmail.com>
2023-04-01 23:58:45 -0300

    feat: improve error validations and handlers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
-const app = require('./app'); // after the env variables load, because needs values
+process.on('uncaughtException', (err) => {
+  console.error('uncaughtException', err.name, err.message);
+  process.exit(1);
+});
 
+dotenv.config();
+const app = require('./app');
+// after the env variables load, because needs values
 module.exports = () => {
   mongoose
     .connect(process.env.DB_CONNECTION_STRING, {
@@ -17,7 +22,15 @@ module.exports = () => {
 
   const port = process.env.PORT || 3000;
 
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
+  });
+
+  process.on('unhandledRejection', (err) => {
+    console.error('unhandledRejection', err.name, err.message);
+    server.close(() => {
+      console.log('Server closed!');
+      process.exit(1);
+    });
   });
 };
--- utils/errorHandleMiddleware.js
+++ utils/errorHandleMiddleware.js
@@ -1,3 +1,22 @@
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  err.message = message;
+  err.statusCode = 400;
+};
+
+const handleDuplicatedFieldsError = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicated field value ${value}. Please use another value`;
+  err.message = message;
+  err.statusCode = 400;
+};
+
+const handleValidationError = (err) => {
+  const message = `Invalid input data`;
+  err.message = message;
+  err.statusCode = 400;
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -7,14 +26,24 @@ module.exports = (err, req, res, next) => {
   let response = {
     status: err.status,
     message: err.message,
-    ...(process.env.NODE_ENV === 'development' && { err, stack: err.stack }),
+    ...(process.env.NODE_ENV === 'development' && {
+      err,
+      stack: err.stack,
+    }),
   };
 
-  if (err.isOperational && process.env.NODE_ENV === 'production')
-    response = {
-      status: 500,
-      message: 'Sorry, something bad happened!',
-    };
+  if (process.env.NODE_ENV === 'production') {
+    if (err.isOperational) {
+      response = {
+        status: 500,
+        message: 'Sorry, something bad happened!',
+      };
+    } else {
+      if (err.name === 'CastError') handleCastError(err);
+      if (err.code === 11000) handleDuplicatedFieldsError(err);
+      if (err.name === 'ValidationError') handleValidationError(err);
+    }
+  }
 
   res.status(err.statusCode).json(response);
 };
