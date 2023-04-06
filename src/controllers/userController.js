5efe5776e2eaeb24924a62eed92626f85891bb9b
jovanibrasil <jovanibrasil@gmail.com>
2023-04-06 08:38:55 -0300

    feat: add user update route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,7 @@
+const AppError = require('../../utils/AppError');
+const catchAsync = require('../../utils/catchAsync');
+const User = require('../models/userModel');
+
 exports.getUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -26,9 +30,21 @@ exports.deleteUser = (req, res) => {
   });
 };
 
-exports.patchUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet!',
+exports.patchUser = catchAsync(async (req, res, next) => {
+  const { password, passwordConfirm, name } = req.body;
+
+  if (password || passwordConfirm)
+    return next(new AppError('This route is not for password updates.', 400));
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { name },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
-};
+});
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
+const jwtValidationMiddleware = require('../../utils/jwtValidationMiddeware');
 
 router.route('/').get(userController.getUsers).post(userController.createUser);
 
 router
   .route('/:id')
   .get(userController.getUser)
-  .patch(userController.patchUser)
+  .patch(jwtValidationMiddleware, userController.patchUser)
   .delete(userController.deleteUser);
 
 module.exports = router;
