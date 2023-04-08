85f5334870a7573b5c92f481064768bdc57e21b1
jovanibrasil <jovanibrasil@gmail.com>
2023-04-08 07:43:54 -0300

    ref: remove unused controller functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,27 +2,6 @@ const AppError = require('../../utils/AppError');
 const catchAsync = require('../../utils/catchAsync');
 const User = require('../models/userModel');
 
-exports.getUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet!',
-  });
-};
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet!',
-  });
-};
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet!',
-  });
-};
-
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
